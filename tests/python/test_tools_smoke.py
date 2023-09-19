cad8a9d0c82ed26d9e23d90381a317bbb9e9cab8
Daniel Gomez <da.gomez@samsung.com>
2023-09-19 14:32:50 +0200

    blkalgn: add block command alignment observability tool
    The tool observes block commands and checks for LBA and block size
    alignment.
    
    The tool is used as part of the Large block size (LBS) effort [1] in the
    kernel to validate min order mapping [2].
    
    [1] https://kernelnewbies.org/KernelProjects/large-block-size
    
    [2] min order:
    use of min order: https://github.com/linux-kdevops/linux/commit/563cea74e999c959cdec86a9022865cd3dc7b12f
    add min order support: https://github.com/linux-kdevops/linux/commit/27f85d8c91af9c9bc5f5481b38ee77523ad4de30
    upstream RFC: https://lore.kernel.org/all/20230915183848.1018717-2-kernel@pankajraghav.com/
    
    Signed-off-by: Daniel Gomez <da.gomez@samsung.com>

--- tests/python/test_tools_smoke.py
+++ tests/python/test_tools_smoke.py
@@ -274,6 +274,10 @@ class SmokeTests(TestCase):
         else:
             self.skipTest("skipped 'nfs module not loaded'")
 
+    @skipUnless(kernel_version_ge(4,19), "requires kernel >= 4.19")
+    def test_blkalgn(self):
+        self.run_with_duration("blkalgn.py")
+
     @skipUnless(kernel_version_ge(4,6), "requires kernel >= 4.6")
     @mayFail("This fails on github actions environment, and needs to be fixed")
     def test_offcputime(self):
